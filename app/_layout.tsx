1a5cf88672d954ffd7a0c74a4f14b9c73432db08
quotalab-tim <hyunchul.yang@gmail.com>
2025-12-03 00:50:59 +0900

    feat: add splash screen feature

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,23 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Asset } from "expo-asset";
+import Constants from "expo-constants";
 import { Stack } from "expo-router";
 import * as SecureStore from "expo-secure-store";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
+import { Alert, Animated, StyleSheet, View } from "react-native";
+
+// Instruct SplashScreen not to hide yet, we want to do this manually
+SplashScreen.preventAutoHideAsync().catch(() => {
+  /* reloading the app might trigger some race conditions, ignore them */
+});
 
 export interface User {
   id: string;
   name: string;
-  description: string;
   profileImageUrl: string;
+  description: string;
   link?: string;
   showInstagramBadge?: boolean;
   isPrivate?: boolean;
@@ -19,66 +28,171 @@ export const AuthContext = createContext<{
   login?: () => Promise<any>;
   logout?: () => Promise<any>;
   updateUser?: (user: User) => void;
-}>({ user: null });
+}>({
+  user: null,
+});
 
-export default function RootLayout() {
+function AnimatedAppLoader({
+  children,
+  image,
+}: {
+  children: React.ReactNode;
+  image: number;
+}) {
   const [user, setUser] = useState<User | null>(null);
-  const login = async () => {
-    await fetch("/login", {
+  const [isSplashReady, setSplashReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      await Asset.loadAsync(image);
+      setSplashReady(true);
+    }
+    prepare();
+  }, [image]);
+
+  const login = () => {
+    console.log("login");
+    return fetch("/login", {
       method: "POST",
       body: JSON.stringify({
-        email: "test@test.com",
+        username: "test@test.com",
         password: "test",
       }),
     })
-    .then(async (res) => {
-      if(res.status >= 400) {
-        throw new Error("Invalid credentials");
-      }
-      const data = await res.json();
-      setUser(data.user);
-      await Promise.all([
-        SecureStore.setItemAsync("accessToken", data.accessToken),
-        SecureStore.setItemAsync("refreshToken", data.refreshToken),
-        AsyncStorage.setItem("user", JSON.stringify(data.user)),
-      ]);
-    })
-    .catch(error => {
-      console.error(error);
-      throw error;
-    });
+      .then((res) => {
+        console.log("res", res, res.status);
+        if (res.status >= 400) {
+          return Alert.alert("Error", "Invalid credentials");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        setUser(data.user);
+        return Promise.all([
+          SecureStore.setItemAsync("accessToken", data.accessToken),
+          SecureStore.setItemAsync("refreshToken", data.refreshToken),
+          AsyncStorage.setItem("user", JSON.stringify(data.user)),
+        ]);
+      })
+      .catch(console.error);
   };
 
-  const logout = async () => {
+  const logout = () => {
     setUser(null);
-    await Promise.all([
+    return Promise.all([
       SecureStore.deleteItemAsync("accessToken"),
       SecureStore.deleteItemAsync("refreshToken"),
       AsyncStorage.removeItem("user"),
     ]);
   };
 
-  const updateUser = (user: User) => {
+  const updateUser = (user: User | null) => {
     setUser(user);
-    AsyncStorage.setItem("user", JSON.stringify(user));
+    if (user) {
+      AsyncStorage.setItem("user", JSON.stringify(user));
+    } else {
+      AsyncStorage.removeItem("user");
+    }
   };
 
-  useEffect(() => {
-    AsyncStorage.getItem("user").then(user => {
-      if(user) {
-        setUser(user ? JSON.parse(user) : null);
-      }
-    });
-    // TODO: validate access token
-  }, []);
-
+  if (!isSplashReady) {
+    return null;
+  }
 
   return (
     <AuthContext value={{ user, login, logout, updateUser }}>
-      <StatusBar style="auto" animated />
+      <AnimatedSplashScreen image={image}>{children}</AnimatedSplashScreen>
+    </AuthContext>
+  );
+}
+
+function AnimatedSplashScreen({
+  children,
+  image,
+}: {
+  children: React.ReactNode;
+  image: number;
+}) {
+  const [isAppReady, setAppReady] = useState(false);
+  const [isSplashAnimationComplete, setAnimationComplete] = useState(false);
+  const animation = useRef(new Animated.Value(1)).current;
+  const { updateUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (isAppReady) {
+      Animated.timing(animation, {
+        toValue: 0,
+        duration: 2000,
+        useNativeDriver: true,
+      }).start(() => setAnimationComplete(true));
+    }
+  }, [isAppReady]);
+
+  const onImageLoaded = async () => {
+    try {
+      // 데이터 준비
+      await Promise.all([
+        AsyncStorage.getItem("user").then((user) => {
+          updateUser?.(user ? JSON.parse(user) : null);
+        }),
+        // TODO: validating access token
+      ]);
+      await SplashScreen.hideAsync();
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setAppReady(true);
+    }
+  };
+
+  const rotateValue = animation.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
+  return (
+    <View style={{ flex: 1 }}>
+      {isAppReady && children}
+      {!isSplashAnimationComplete && (
+        <Animated.View
+          pointerEvents="none"
+          style={[
+            {
+              ...StyleSheet.absoluteFillObject,
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor:
+                Constants.expoConfig?.splash?.backgroundColor || "#ffffff",
+              opacity: animation,
+            },
+          ]}
+        >
+          <Animated.Image
+            source={image}
+            style={{
+              resizeMode: Constants.expoConfig?.splash?.resizeMode || "contain",
+              width: Constants.expoConfig?.splash?.imageWidth || 200,
+              transform: [{ scale: animation }, { rotate: rotateValue }],
+            }}
+            onLoadEnd={onImageLoaded}
+            fadeDuration={0}
+          />
+        </Animated.View>
+      )}
+    </View>
+  );
+}
+
+export default function RootLayout() {
+  return (
+    <AnimatedAppLoader image={require("../assets/images/react-logo.png")}>
+      <StatusBar style="auto" animated hidden={false} />
       <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="(tabs)" />
         <Stack.Screen name="modal" options={{ presentation: "modal" }} />
       </Stack>
-    </AuthContext>
+    </AnimatedAppLoader>
   );
 }
