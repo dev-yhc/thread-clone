5df906283bb8cb6099cbad076e36405deab4efb5
quotalab-tim <hyunchul.yang@gmail.com>
2025-12-12 22:29:37 +0900

    feat: create dummy data with miragejs

--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
+import { faker } from "@faker-js/faker";
 import "expo-router/entry";
-import { createServer, Server } from "miragejs";
+import {
+    belongsTo,
+    createServer,
+    Factory,
+    hasMany,
+    Model,
+    Response,
+    RestSerializer,
+    Server,
+} from "miragejs";
+import { type User } from "./app/_layout";
+
+let yhc: User
 
 declare global {
     interface Window {
@@ -8,12 +21,96 @@ declare global {
 }
 
 if (__DEV__) {
-    if(window.server) {
+    if (window.server) {
         window.server.shutdown();
     }
 
     window.server = createServer({
+        models: {
+            user: Model.extend({
+                posts: hasMany("post"),
+                activities: hasMany("activity"),
+            }),
+            post: Model.extend({
+                user: belongsTo("user"),
+            }),
+            activity: Model.extend({
+                user: belongsTo("user"),
+            }),
+        },
+        serializers: {
+            post: RestSerializer.extend({
+                include: ["user"],
+                embed: true,
+            }),
+            activity: RestSerializer.extend({
+                include: ["user"],
+                embed: true,
+            }),
+        },
+        factories: {
+            user: Factory.extend({
+                id: () => faker.person.firstName(),
+                name: () => faker.person.fullName(),
+                description: () => faker.lorem.sentence(),
+                profileImageUrl: () =>
+                    `https://avatars.githubusercontent.com/u/${Math.floor(
+                        Math.random() * 100_000
+                    )}?v=4`,
+                isVerified: () => Math.random() > 0.5,
+            }),
+            post: Factory.extend({
+                id: () => faker.string.numeric(6),
+                content: () => faker.lorem.paragraph(),
+                imageUrls: () =>
+                    Array.from({ length: Math.floor(Math.random() * 3) }, () =>
+                        faker.image.urlLoremFlickr()
+                    ),
+                likes: () => Math.floor(Math.random() * 100),
+                comments: () => Math.floor(Math.random() * 100),
+                reposts: () => Math.floor(Math.random() * 100),
+            }),
+        },
+        seeds(server) {
+            yhc = server.create("user", {
+                id: "yhc",
+                name: "yhc",
+                description: "🐢 lover, programmer, youtuber",
+                profileImageUrl: "https://avatars.githubusercontent.com/u/885857?v=4",
+            });
+            const users = server.createList("user", 10);
+            users.forEach((user) => {
+                server.createList("post", 5, {
+                    user,
+                });
+            });
+        },
         routes() {
+            this.post("/posts", (schema, request) => {
+                const { posts } = JSON.parse(request.requestBody);
+                posts.forEach((post: any) => {
+                    schema.create("post", {
+                        content: post.content,
+                        imageUrls: post.imageUrls,
+                        location: post.location,
+                        user: schema.find("user", "zerohch0"),
+                    });
+                });
+                return new Response(200, {}, { posts });
+            });
+
+            this.get("/posts", (schema, request) => {
+                console.log("user.all", schema.all("user").models);
+                const cursor = parseInt((request.queryParams.cursor as string) || "0");
+                const posts = schema.all("post").models.slice(cursor, cursor + 10);
+                return new Response(200, {}, { posts });
+            });
+
+            this.get("/posts/:id", (schema, request) => {
+                const post = schema.find("post", request.params.id);
+                const comments = schema.all("post").models.slice(0, 10);
+                return new Response(200, {}, { post, comments });
+            });
             this.post("login", (schema, request) => {
                 const { email, password } = JSON.parse(request.requestBody);
                 if (email === "test@test.com" && password === "test") {
@@ -28,7 +125,7 @@ if (__DEV__) {
                         }
                     };
                 }
-                return new Response(JSON.stringify({ error: "Invalid credentials" }), { status: 401 });
+                return new Response(401, {}, { error: "Invalid credentials" });
             });
         },
     });
